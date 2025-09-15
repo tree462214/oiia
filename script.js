97a8ec4a605fd313c51531f7a93096bf5c47f5a3
tree462214 <87408369+tree462214@users.noreply.github.com>
2025-09-15 14:07:07 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ function changeText() {
     // HTML에서 id가 'description'인 요소를 찾아서 변수에 저장
     const element = document.getElementById('description');
     const img = document.getElementById('img');
+    const myAudio = document.getElementById('myAudio')
     // 해당 요소의 내용을 변경
     element.textContent = 'oiiiaioiiaioiiiai'
-    img.src = 'oiia2.gif'
+    img.src = 'data/oiia2.gif'
+    myAudio.play();
     setTimeout(function () {
         element.textContent = 'oiiai';
-        img.src = 'oiia1.webp'
+        img.src = 'data/oiia1.webp'
+        myAudio.pause();
     },1500)
     
 }
