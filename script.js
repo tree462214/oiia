fb2d8454f204134e9036c80dfef500c1d8335463
tree462214 <87408369+tree462214@users.noreply.github.com>
2025-09-15 16:56:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function changeText() {
     // 해당 요소의 내용을 변경
     element.textContent = 'oiiiaioiiaioiiiai'
     img.src = 'oiia2.gif'
-    myAudio.currentTime = 2
+    myAudio.currentTime = 0.1
     myAudio.play();
     setTimeout(function () {
         element.textContent = 'oiiai';
@@ -16,6 +16,7 @@ function changeText() {
     
 
 }
+
 
 
 
